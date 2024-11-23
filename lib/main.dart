26003a901157d318f72dcb41f7b6667404a07bdb
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-11-24 01:28:16 +0530

    App Bar and its basic properties

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.pink,
+        backgroundColor: Colors.pink[300],
+        appBar: AppBar(
+          title: Text(
+            "My App Bar",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: Colors.deepPurple,
+          elevation: 0, //to control the shadow  below the appbar
+          leading: Icon(Icons.menu), //to put icon on the left hand side
+          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))], //to put an icon on the right hand side, it requires a list
+        ),
         body: Center(
           child: Container(
             height: 300,
@@ -32,11 +45,14 @@ class MyApp extends StatelessWidget {
             ),  //Text
 
              */ //Text
+            /*
             child: Icon(
               Icons.favorite,
               color: Colors.white,
               size: 64,
             ),
+
+             */ //Icon
           ),
         ),
       ),
